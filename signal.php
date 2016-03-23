aa3aec2e44fd9dfb648052147303acd23adb6886
BentabetAbdelDjalil <bentabetj@yahoo.fr>
2016-03-23 18:36:13 +0100

    interface web page

---
+++ signal.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+        <title>switcher</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  
+  </head>
+  <body>
+       
+          <nav class="navbar navbar-inverse" role="navigation">
+            <div class="container-fluid">
+  
+              <div class="navbar-header">
+               
+                <a class="navbar-brand" href="#">Switch ON/OFF Ac light (Rspberry pi + Arduino + Server) </a>
+              </div>
+            </nav>
+
+           <div class="row">
+            <div class="col-lg-offset-4 col-lg-3">
+            <form  action="execute.php" method="post">
+             <div  class="form-group">
+               <div>
+                <input class="form-control" name="name"type="text">
+                    
+               </div>
+               </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+               </form>
+             </div>
+           </div>
+         
+
+
+
+    </div>   
+   
+   
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="https//code.jquery.com/jquery.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
